1e54b04b999deaff5cf4d960411fc8f41f010c74
Nurra Aulia <nurrato10@gmail.com>
2025-07-24 05:15:08 +0700

    Tambah Landing Page dan Halaman MBTI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MBTI Page</title>
 
-    <!-- Tambahkan ini -->
     <link rel="stylesheet" href="{{ asset('css/mbti.css') }}">
     <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
 
